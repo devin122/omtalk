0c6e3e6f425e96fad9b2b0c5b166340a3d289d84
Devin Nakamura <devin122@gmail.com>
2020-09-24 23:11:46 +0000

    Now with 30% more hacks, for her pleasure!

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -3,5 +3,6 @@ set -xe
 
 mkdir build
 cd build
-cmake -GNinja -DMLIR_ROOT_DIR=/local/llvm -C ../cmake/caches/all-dev.cmake ..
+cmake -GNinja -DMLIR_ROOT_DIR=/local/llvm -C ../cmake/caches/gc-dev.cmake -DOMTALK_SPLIT_DEBUG=OFF ..
 ninja
+ctest --output-on-failure
